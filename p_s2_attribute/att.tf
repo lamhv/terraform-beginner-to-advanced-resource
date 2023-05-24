99accbb57729e235286ab66bc046a9cb2b1b7853
lamhv <hoangvietlambk@gmail.com>
2023-05-24 18:28:34 +0700

    prace attribute value

--- p_s2_attribute/att.tf
+++ p_s2_attribute/att.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.27.0"
+      version = "4.64.0"
     }
   }
 }
 
 provider "google" {
-  project = "fb-crawler-331309"
+  project = "data-engineering-230006"
   zone = "asia-southeast1-a"
   region = "asia-southeast1"
 }
@@ -19,7 +19,7 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-11"
     }
   }
 
@@ -36,7 +36,7 @@ resource "google_compute_instance" "vm_instance" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
+  name                    = "lamhoang-terraform-network"
   auto_create_subnetworks = "false"
 }
 
@@ -45,14 +45,14 @@ resource "google_compute_address" "static" {
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
-  name          = "my-subnet"
+  name          = "lamhoang-my-subnet"
   ip_cidr_range = "10.0.0.0/16"
   region = "asia-southeast1"
   network       = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_address" "internal_with_subnet_and_address" {
-  name         = "my-internal-address"
+  name         = "lamhoang-my-internal-address"
   subnetwork   = google_compute_subnetwork.subnetwork.id
   address_type = "INTERNAL"
   address      = "10.0.42.42"
