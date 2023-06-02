16898295bcd94594176f40267eb040426f270b62
lamhv <hoangvietlambk@gmail.com>
2023-06-02 09:19:44 +0700

    separate variable value into terraform.tfvars

---
+++ p_s2_attribute/terraform.tfvars
@@ -0,0 +1,3 @@
+subnet_range_ip = "10.0.0.0/16"
+private_ip = "10.0.42.42"
+range_ports = ["80", "8080", "1000-2000"]
--- p_s2_attribute/var.tf
+++ p_s2_attribute/var.tf
@@ -1,13 +1,12 @@
 variable "subnet_range_ip" {
-  default = "10.0.0.0/16"
+  type = string
 }
 
 // internal IP address
 variable "private_ip" {
-  default = "10.0.42.42"
+  type    = string
 }
 
 variable "range_ports" {
   type    = list(string)
-  default = ["80", "8080", "1000-2000"]
 }
